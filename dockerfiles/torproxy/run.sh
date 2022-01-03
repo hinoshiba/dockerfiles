23fdcdf8c0c7750d8c4ea5b617c6b9f047b78e67
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2022-01-03 21:14:08 +0900

    updated messages

--- dockerfiles/torproxy/run.sh
+++ dockerfiles/torproxy/run.sh
@@ -4,6 +4,6 @@ service tor start || exit 1
 echo "[[[Network Infomation]]]"
 curl -x socks5h://0:9150 http://ipinfo.io || exit 1
 
-echo "[[[USAGE]]]"
+echo -e "\n[[[USAGE]]]"
 echo 'USAGE: open "/Applications/<browser>.app" --args --proxy-server="socks5://localhost:9150'
 echo 'USAGE: curl -x socks5h://0:9150 <target url>'
