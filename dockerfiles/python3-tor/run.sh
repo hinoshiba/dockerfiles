fb93264f91d19a0d8f7eb8d2de9944aeec693ad4
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2022-01-03 21:11:41 +0900

    updated messages

--- dockerfiles/python3-tor/run.sh
+++ dockerfiles/python3-tor/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 service tor start || exit 1
+echo "[[[Network Infomation]]]"
 curl -x socks5h://0:9150 http://ipinfo.io || exit 1
 
-echo "connected!!"
+echo "[[[USAGE]]]"
 echo 'USAGE: open "/Applications/<browser>.app" --args --proxy-server="socks5://localhost:9150'
 echo 'USAGE: curl -x socks5h://0:9150 <target url>'
