758b7dec0982a6745770148c90eaa7c131db1736
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2022-01-04 19:48:31 +0900

    apply local uid

---
+++ dockerfiles/workbench/add_local_user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USER_ID=${LOCAL_UID:-9001}
+GROUP_ID=${LOCAL_GID:-9001}
+
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+useradd -u $USER_ID -o -m wrkkk
+groupmod -g $GROUP_ID wrkkk
+export HOME=/home/wrkkk
+
+su -u user "$@"
