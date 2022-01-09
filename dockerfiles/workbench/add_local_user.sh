987ed6e1c9b5ff12660f50e71d505b3dec36cca2
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2022-01-09 11:27:40 +0900

    fixed

--- dockerfiles/workbench/add_local_user.sh
+++ dockerfiles/workbench/add_local_user.sh
@@ -7,10 +7,12 @@ echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
 useradd -u $USER_ID -o -m ${LOCAL_WHOAMI}
 groupmod -g $GROUP_ID ${LOCAL_WHOAMI}
 
+rm /home/${LOCAL_WHOAMI}/.bashrc
 ln -s /mnt/${LOCAL_HOME}/.bashrc /home/${LOCAL_WHOAMI}/.bashrc
 ln -s /mnt/${LOCAL_HOME}/.vimrc /home/${LOCAL_WHOAMI}/.vimrc
 ln -s /mnt/${LOCAL_HOME}/.ssh /home/${LOCAL_WHOAMI}/.ssh
 ln -s /mnt/${LOCAL_HOME}/.screenrc /home/${LOCAL_WHOAMI}/.screenrc
 ln -s /mnt/${LOCAL_HOME}/Downloads /home/${LOCAL_WHOAMI}/Downloads
+ln -s /mnt/${LOCAL_HOME}/share /home/${LOCAL_WHOAMI}/share
 
-exec su -l -s /bin/bash - ${LOCAL_WHOAMI}
+su -l -s /bin/bash - ${LOCAL_WHOAMI}
