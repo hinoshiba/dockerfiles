a47f40d6a88facdc667c4697bbf8d92d8ea626d9
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2022-03-11 15:29:16 +0900

    fixed close session when empty repository.

--- dockerfiles/workbench/scripts/clone.sh
+++ dockerfiles/workbench/scripts/clone.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -eu
 
+SHELL="/bin/bash"
 PATH_ROOT="${HOME}/git/"
 
 if [ $# -ne 1 ]; then
 	echo "usage: clone.sh <remote repository>"
-	exec bash
+	exec ${SHELL}
 fi
 
 target=$(echo "${1}" | sed -e 's/^.*@//' -e 's/:/\//' -e 's/\.git$//')
 
 target_dir=$(dirname "${PATH_ROOT}${target}")
 echo "${PATH_ROOT}${target}"
-test -d "${PATH_ROOT}${target}" || (mkdir -p ${target_dir}; cd "${target_dir}"; git clone "${1}")
-cd "${PATH_ROOT}${target}" && git pull
+test -d "${PATH_ROOT}${target}" || (mkdir -p ${target_dir}; cd "${target_dir}"; git clone "${1}") || exec ${SHELL}
+cd "${PATH_ROOT}${target}" && git pull || exec ${SHELL}
 cd "${PATH_ROOT}${target}"
-exec bash
+exec ${SHELL}
