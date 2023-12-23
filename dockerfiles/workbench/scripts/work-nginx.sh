3a486b5e29a5ab2506ee494b2d6ad8225a656653
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2023-12-23 13:22:14 +0900

    updated

--- dockerfiles/workbench/scripts/work-nginx.sh
+++ dockerfiles/workbench/scripts/work-nginx.sh
@@ -8,9 +8,9 @@ DIR=$(pwd)
 target="nginx"
 
 if [[ "$(pwd)" =~ "${HOME}/git/".*$ ]]; then
-	cd "${DOCKERFILE_PATH}" && make target=${target} mount=${HOME}/git workdir="${DIR}" port=80
+	cd "${DOCKERFILE_PATH}" && make target=${target} mount=${HOME}/git workdir="${DIR}" port=80 root=y
 else
-	cd "${DOCKERFILE_PATH}" && make target=${target} mount=${HOME}/git port=80
+	cd "${DOCKERFILE_PATH}" && make target=${target} mount=${HOME}/git port=80 root=y
 fi
 make stop target=${target} || :
 cd "${DIR}"
